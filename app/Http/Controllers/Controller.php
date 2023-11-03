64e7f6636e436a1e54a179f4db8e03f110b0b884
Kukuh182 <kukuhpr21@gmail.com>
2023-11-03 14:49:36 +0700

    add session flash

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,11 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function createSessionFlash($request, $action_message, $status)
+    {
+        $section = ($status) ? 'success' : 'failed';
+        $startMessage = ucwords($section);
+        $request->session()->flash($section, $startMessage . ' ' . $action_message);
+    }
 }
