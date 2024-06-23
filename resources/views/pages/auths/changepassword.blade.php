356016ac2babadc28eaa68b5ff59f1a62205a04a
Kukuh Prakoso <kukuhpr21@gmail.com>
2024-06-23 08:58:22 +0700

    title change password from config env

--- resources/views/pages/auths/changepassword.blade.php
+++ resources/views/pages/auths/changepassword.blade.php
@@ -9,7 +9,7 @@
                 <!-- Logo -->
                 <div class="app-brand justify-content-center">
                     <a href="{{ url('login') }}" class="app-brand-link gap-2">
-                        <span class="app-brand-text demo text-body fw-bolder">Meeting Room</span>
+                        <span class="app-brand-text demo text-body fw-bolder">{{ env('APP_NAME') }}</span>
                     </a>
                 </div>
                 <!-- /Logo -->
